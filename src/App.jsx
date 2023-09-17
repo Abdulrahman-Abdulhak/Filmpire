96de88119a6f3b6fab513bf5a44a8b7c7b083bb5
Abdulrahman Abdulhak <bdalrhmanbdalhq@gmail.com>
2023-09-17 04:45:37 -0700

    project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Movies, MovieInformation, Actors, Profile } from './pages';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="movie/:id" element={<MovieInformation />} />
+          <Route path="actors/:id" element={<Actors />} />
+          <Route path="profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
