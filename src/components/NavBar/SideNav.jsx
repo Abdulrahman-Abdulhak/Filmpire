78719378e85e13209719e0c2e39e3683d8264429
maramkaiser <maramkaiser4@gmail.com>
2023-11-14 18:12:22 +0300

    nothing

--- src/components/NavBar/SideNav.jsx
+++ src/components/NavBar/SideNav.jsx
@@ -1,34 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import { useTheme } from '@emotion/react';
-import { Box, CircularProgress, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader, useMediaQuery } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useTheme } from "@emotion/react";
+import {
+  Box,
+  CircularProgress,
+  Divider,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  useMediaQuery,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-import useStyles from './style';
-import catGenres from '../../assets/cat_genre';
-import { useGetGenresQuery } from '../../services/TMDB';
-import { selectCatOrGenre } from '../../features/currentCatOrGenre';
+import useStyles from "./style";
+import catGenres from "../../assets/cat_genre";
+import { useGetGenresQuery } from "../../services/TMDB";
+import { selectCatOrGenre } from "../../features/currentCatOrGenre";
 
 const categories = [
-  { label: 'Popular', value: 'popular' },
-  { label: 'Top Rated', value: 'top_rated' },
-  { label: 'Upcoming', value: 'upcoming' },
+  { label: "Popular", value: "popular" },
+  { label: "Top Rated", value: "top_rated" },
+  { label: "Upcoming", value: "upcoming" },
 ];
 
 function SideNav({ menu }) {
   const classes = useStyles();
   const theme = useTheme();
-  const isMobile = useMediaQuery('(max-width:600px)');
+  const isMobile = useMediaQuery("(max-width:600px)");
 
   const dispatch = useDispatch();
   const { data, isLoading } = useGetGenresQuery();
   const navigate = useNavigate();
 
+  // console.log(JSON.stringify(data.genres))
+
   const selectNav = (val) => {
     menu.toggleMenu(false);
     dispatch(selectCatOrGenre(val));
-    navigate('/', {
+    navigate("/", {
       replace: true,
     });
   };
@@ -37,17 +50,19 @@ function SideNav({ menu }) {
     <Drawer
       classes={{ paper: classes.sidePaper }}
       className={classes.sideNav}
-      variant={isMobile ? 'temporary' : 'permanent'}
-      anchor={isMobile ? 'right' : 'left'}
+      variant={isMobile ? "temporary" : "permanent"}
+      anchor={isMobile ? "right" : "left"}
       open={isMobile ? menu.menuOpened : true}
       ModalProps={{ keepMounted: true }}
       onClose={() => menu.toggleMenu(false)}
     >
       <Link to="/" onClick={() => menu.toggleMenu(false)}>
         <img
-          src={theme.palette.mode === 'dark'
-            ? 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png'
-            : 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png'}
+          src={
+            theme.palette.mode === "dark"
+              ? "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png"
+              : "https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png"
+          }
           alt="site's logo"
         />
       </Link>
@@ -55,7 +70,7 @@ function SideNav({ menu }) {
       <div>
         <List>
           <ListSubheader>Categories</ListSubheader>
-          {categories.map((({ label, value }) => (
+          {categories.map(({ label, value }) => (
             <ListItemButton
               key={`cat:${value}`}
               onClick={() => selectNav(value)}
@@ -65,30 +80,27 @@ function SideNav({ menu }) {
               </ListItemIcon>
               <ListItemText primary={label} />
             </ListItemButton>
-          )))}
+          ))}
         </List>
       </div>
       <Divider />
       <div>
         <List>
           <ListSubheader>Genres</ListSubheader>
-          {
-            isLoading ? (
-              <Box display="flex" justifyContent="center" alignItems="center">
-                <CircularProgress />
-              </Box>
-            ) : data.genres.map((({ id, name }) => (
-              <ListItemButton
-                key={`genre:${id}`}
-                onClick={() => selectNav(id)}
-              >
+          {isLoading ? (
+            <Box display="flex" justifyContent="center" alignItems="center">
+              <CircularProgress />
+            </Box>
+          ) : (
+            data.genres.map(({ id, name }) => (
+              <ListItemButton key={`genre:${id}`} onClick={() => selectNav(id)}>
                 <ListItemIcon className={classes.listItem}>
                   <img src={catGenres[name.toLowerCase()]} alt="" />
                 </ListItemIcon>
                 <ListItemText primary={name} />
               </ListItemButton>
-            )))
-          }
+            ))
+          )}
         </List>
       </div>
     </Drawer>
